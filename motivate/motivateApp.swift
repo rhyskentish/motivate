c9269c8a3ee801c072e9769cba8590fcf8c0966b
Rhys Kentish <rhyskentish@gmail.com>
2020-10-26 11:32:47 +0000

    Initial Commit

---
+++ motivate/motivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivateApp.swift
+//  motivate
+//
+//  Created by Rhys Kentish on 26/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct motivateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
